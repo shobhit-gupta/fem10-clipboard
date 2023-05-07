f9b57364cb0d02c086f0681af097e583006eb2a3
Shobhit Gupta <25628519+shobhit-gupta@users.noreply.github.com>
2023-05-07 15:35:46 +0000

    feat: Add hover effect for Download buttons

--- src/lib/UI/DownloadButton.svelte
+++ src/lib/UI/DownloadButton.svelte
@@ -2,11 +2,15 @@
 	export let url: string;
 	export let bg: string = 'bg-black';
 	export let fg: string = 'text-white';
+	export let hoverColor: string = 'bg-white/25';
 </script>
 
 <a
 	href={url}
 	class="
+        group
+        relative
+        overflow-hidden
         px-9
         py-3
         rounded-full
@@ -22,5 +26,6 @@
         border-black/10
     "
 >
+	<div class="absolute top-0 left-0 bottom-0 right-0 {hoverColor} opacity-0 hover:opacity-100" />
 	<slot />
 </a>
